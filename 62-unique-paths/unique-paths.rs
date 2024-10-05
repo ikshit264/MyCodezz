63b192a120cad62645789f792c952ea074e75eff
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2024-10-05 21:34:37 +0530

    Time: 0 ms (100.00%) | Memory: 2.1 MB (86.86%) - LeetSync

--- 62-unique-paths/unique-paths.rs
+++ 62-unique-paths/unique-paths.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 impl Solution {
         pub fn unique_paths(m: i32, n: i32) -> i32 {
         let mut dp = vec![1; n as usize]; 
-        for _ in 1..m {
+        for i in 1..m {
             for j in 1..n {
                 dp[j as usize] += dp[(j - 1) as usize];
             }
