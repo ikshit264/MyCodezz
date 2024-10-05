37c2a5e8736be9df928008031d4930a17c6d672d
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2024-10-05 19:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 2.1 MB (86.86%) - LeetSync

--- 62-unique-paths/unique-paths.rs
+++ 62-unique-paths/unique-paths.rs
@@ -1,28 +1,14 @@
 use std::collections::HashMap;
 
 impl Solution {
-
-    fn grid_traversal(m : i32, n:i32, memo :&mut HashMap<(i32, i32), i32>)->i32{
-        if let Some(&value) = memo.get(&(m, n)){
-            return value;
+        pub fn unique_paths(m: i32, n: i32) -> i32 {
+        let mut dp = vec![1; n as usize]; 
+        for _ in 1..m {
+            for j in 1..n {
+                dp[j as usize] += dp[(j - 1) as usize];
+            }
         }
 
-        if n == m && n == 1{
-            return 1;
-        }
-
-        if n == 0 || m == 0{
-            return 0;
-        }
-
-        let res = Self::grid_traversal(m-1, n, memo) + Self::grid_traversal(m, n-1, memo);
-        memo.insert((m,n), res);
-        res
-    }
-
-    pub fn unique_paths(m: i32, n: i32) -> i32 {
-        let mut memo = HashMap::new();
-
-        return Self::grid_traversal(m, n,&mut memo);
+        dp[(n - 1) as usize] 
     }
 }
