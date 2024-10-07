a538d6f7d2ac86c87a3fbc13b3c7379efc3bd5bd
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2024-10-07 21:14:36 +0530

    Time: 24 ms (52.38%) | Memory: 3.9 MB (12.70%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.rs
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.rs
@@ -2,7 +2,6 @@ impl Solution {
     pub fn can_partition(nums: Vec<i32>) -> bool {
         let total_sum: i32 = nums.iter().sum();
 
-        // If total sum is odd, we cannot partition it into two equal subsets
         if total_sum % 2 != 0 {
             return false;
         }
@@ -11,11 +10,10 @@ impl Solution {
         let n: usize = nums.len();
         let mut dp: Vec<Vec<bool>> = vec![vec![false; (target + 1) as usize]; n + 1];
 
-        // Initialize the dp table
         for i in 0..=n {
-            dp[i][0] = true; // With zero items, sum of 0 is always possible
+            dp[i][0] = true;
         }
-
+        
         for i in 1..=n {
             for j in 1..=(target as usize) {
                 if nums[i - 1] <= j as i32 {
