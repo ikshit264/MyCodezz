64c16488d96cca1db5e250965947b3739b586bdc
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-01-21 18:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (70.42%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -5,8 +5,7 @@ public:
         long long ans = 1;
         store.push_back(1);
         for (int i = 1; i < rowIndex + 1; i++) {
-            ans *= (rowIndex + 1 - i);
-            ans /= i;
+            ans = (ans*(rowIndex + 1 - i))/i;
             store.push_back(ans);
         }
         return store;
