214994a734d5162e988c85f8053ccb838176ea70
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-01-21 18:17:27 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (47.95%) - LeetSync

---
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> store;
+        long long ans = 1;
+        store.push_back(1);
+        for (int i = 1; i < rowIndex + 1; i++) {
+            ans *= (rowIndex + 1 - i);
+            ans /= i;
+            store.push_back(ans);
+        }
+        return store;
+    }
+};
