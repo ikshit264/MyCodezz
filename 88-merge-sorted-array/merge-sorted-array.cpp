f0a200c875e926bc2560ff091509604dfece0041
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-01-23 16:22:10 +0530

    Time: 4 ms (2.25%) | Memory: 12.5 MB (8.75%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int t = m - 1, b = n - 1;
+        for (int i=m+n-1; i>=0; i--){
+            cout << i;
+            if (b < 0 || (t > -1 && nums1[t] >= nums2[b])){
+                nums1[i] = nums1[t];
+                t--;
+            }
+            else{
+                nums1[i] = nums2[b];
+                b--;
+            }
+        }
+    }
+};
