64cbe661423bb3e456200d54610cbfe0a646c443
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-01-24 10:55:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (90.11%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    int countPaths(int i, int j, int m, int n, vector<vector<int>>& store) {
-        if (i == n - 1 && j == m - 1) return 1; // Reached destination
-        if (i >= n || j >= m) return 0;        // Out of bounds
+    // int countPaths(int i, int j, int m, int n, vector<vector<int>>& store) {
+    //     if (i == n - 1 && j == m - 1) return 1; // Reached destination
+    //     if (i >= n || j >= m) return 0;        // Out of bounds
 
-        if (store[i][j] != -1) return store[i][j];
+    //     if (store[i][j] != -1) return store[i][j];
 
-        store[i][j] = countPaths(i + 1, j, m, n, store) + countPaths(i, j + 1, m, n, store);
-        return store[i][j];
+    //     store[i][j] = countPaths(i + 1, j, m, n, store) + countPaths(i, j +
+    //     1, m, n, store); return store[i][j];
+    // }
+
+    int nCr(int n, int r) {
+        if (r > n - r)
+            r = n - r; // Use the smaller of r or (n - r)
+        long long store = 1;
+        for (int i = 0; i < r; i++) {
+            store *= (n - i);
+            store /= (i + 1);
+        }
+        return (int)store;
     }
 
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> store(n, vector<int>(m, -1)); // Dimensions are n x m
-        return countPaths(0, 0, m, n, store);
-    }
+    int uniquePaths(int m, int n) { return nCr(m + n - 2, n - 1); }
 };
