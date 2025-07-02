a3972991757b70b7fbcba92a3fdbb837efe4389b
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-07-02 14:29:42 +0530

    Time: 0 ms (100%), Space: 17.9 MB (13.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -14,5 +14,9 @@ class Solution:
 
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        store = [-1] * n
-        return self.solve(nums, store, n, 0)
+        store = [-1] * (n+1)
+        store[n] = 0
+        store[n-1] = nums[n-1]
+        for i in range(n-2, -1, -1):
+            store[i] = max(store[i+1], store[i+2] + nums[i])
+        return store[0]
