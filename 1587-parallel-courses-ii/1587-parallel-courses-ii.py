893b7423401bc717bdeb1fd10e26d7f26e414218
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-07-04 01:50:33 +0530

    Time: 803 ms (50.99%), Space: 18.5 MB (86.09%) - LeetHub

---
+++ 1587-parallel-courses-ii/1587-parallel-courses-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minNumberOfSemesters(self, n: int, relations: List[List[int]], k: int) -> int:
+        post = collections.defaultdict(list)
+        degree = [0 for _ in range(n)]
+        for a, b in relations:
+            degree[b - 1] += 1
+            post[a - 1].append(b - 1)
+        
+        @cache
+        def dp(mask: int) -> int:
+            if mask == (1 << n) - 1: return 0
+            take = []
+            for i in range(n):
+                if not mask & (1 << i) and degree[i] == 0: take.append(i)
+
+            ans = float("inf")
+            for k_take in combinations(take, min(k, len(take))):
+                new_mask = mask
+                for i in k_take:
+                    new_mask |= 1 << i
+                    for next in post[i]: degree[next] -= 1
+                ans  = min(ans, 1 + dp(new_mask))
+                for i in k_take:
+                    for next in post[i]: degree[next] += 1
+            return ans
+        
+        return dp(0)
