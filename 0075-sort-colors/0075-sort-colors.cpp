b728f8558238eb965d86b9e684d5ebd77a39a2f0
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-07-10 15:45:00 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count0=0, count1=0, count2=0;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if(nums[i] == 0)
-                count0++;
-            if(nums[i] == 1)
-                count1++;
-            if(nums[i] == 2)
-                count2++;
+        int i=0,j=0,k=nums.size()-1;
+        while(j<=k)
+        {
+            if(nums[j]==0)
+            {
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
+            }
+            else if(nums[j]==1)
+            {
+                j++;
+            }
+            else
+            {
+                swap(nums[j],nums[k]);
+                k--;
+            }
         }
-        cout<<"[";
-        int j=0;
-        for (int i = 0; i < count0; i++) {
-            nums[j]=0;
-            j++;
-            cout << 0 << ",";
-        }
-
-        for (int i = 0; i < count1; i++) {
-            nums[j]=1;
-            j++;
-            cout << 1 << ",";
-        }
-
-        for (int i = 0; i < count2; i++) {
-            nums[j]=2;
-            j++;
-            cout << 2;
-            if(i!=count2-1)
-                cout << ",";
-        }
-        cout << "]";
     }
 };
