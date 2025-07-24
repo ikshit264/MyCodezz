297fc38a4e96b41e1148d365c9957ccf88bf9411
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-07-24 07:41:22 +0530

    Time: 4 ms (60.67%), Space: 47.8 MB (31.37%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -6,6 +6,7 @@ class Solution {
         for(int i = 1; i < n; i++){
             grid[i][0] += grid[i-1][0] ;
         }
+
         for(int i = 1; i < m; i++){
             grid[0][i] += grid[0][i-1];
         }
@@ -14,13 +15,6 @@ class Solution {
             for (int j = 1; j < m; j++) {
                 grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
             }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(grid[i][j] + " ");
-            }
-            System.out.println();
         }
         
         return grid[n-1][m-1];
