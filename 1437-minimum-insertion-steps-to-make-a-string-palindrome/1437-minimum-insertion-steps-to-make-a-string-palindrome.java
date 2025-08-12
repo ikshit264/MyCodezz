e897087bc700c420c3c69c6f2feed8207975cf80
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-08-12 22:07:02 +0530

    Time: 25 ms (66.63%), Space: 45.7 MB (38.27%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,20 +1,32 @@
 class Solution {
-    static String s;
-    int[][] store;
+    // static String s;
+    // int[][] store;
 
-    public int solve(int i, int j){
-        if (i >= j) return 0;
-        if (store[i][j] != -1) return store[i][j];
-        if (s.charAt(i) == s.charAt(j)) return solve(i + 1, j - 1);
+    // public int solve(int i, int j){
+    //     if (i >= j) return 0;
+    //     if (store[i][j] != -1) return store[i][j];
+    //     if (s.charAt(i) == s.charAt(j)) return solve(i + 1, j - 1);
 
-        return store[i][j] = 1 + Math.min(solve(i+1, j), solve(i, j-1));
-    }
+    //     return store[i][j] = 1 + Math.min(solve(i+1, j), solve(i, j-1));
+    // }
 
     public int minInsertions(String s) {
-        this.s = s;
-        store = new int[s.length()][s.length()];
+        String rev = new StringBuilder(s).reverse().toString();
+        int n = s.length();
 
-        for (int[] row : store) java.util.Arrays.fill(row, -1);
-        return solve(0, s.length()-1);
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == rev.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int lps = dp[n][n];
+        return n - lps;
     }
 }
