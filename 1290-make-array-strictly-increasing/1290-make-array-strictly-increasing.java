b1232409328fb1672f7fe3d6f4c8e33cba3abede
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-08-14 09:11:15 +0530

    Time: 936 ms (5.19%), Space: 124 MB (14.93%) - LeetHub

--- 1290-make-array-strictly-increasing/1290-make-array-strictly-increasing.java
+++ 1290-make-array-strictly-increasing/1290-make-array-strictly-increasing.java
@@ -1,6 +1,6 @@
 class Solution {
     final int MAX_INT = 1_000_000_001;
-    int[] arr1, arr2;
+    // int[] arr1, arr2;
     Map<String, Integer> store = new HashMap<>();
 
     private int upperBound(int[] arr, int target){
@@ -14,7 +14,7 @@ class Solution {
         return left;
     }
 
-    public int solve(int i, int prev){
+    public int solve(int i, int[] arr1, int[] arr2, int prev){
         if (i >= arr1.length) return 0;
 
         String key = i + "," + prev;
@@ -23,14 +23,14 @@ class Solution {
         int result1 = MAX_INT;
 
         if (arr1[i] > prev) {
-            result1 = solve(i+1, arr1[i]);
+            result1 = solve(i+1, arr1, arr2, arr1[i]);
         }
 
         int result2 = MAX_INT;
 
         int j = upperBound(arr2, prev);
         if (j < arr2.length)
-            result2 = 1 + solve(i+1, arr2[j]);
+            result2 = 1 + solve(i+1, arr1, arr2, arr2[j]);
 
 
         int ans = Math.min(result1, result2);
@@ -39,10 +39,10 @@ class Solution {
     }
 
     public int makeArrayIncreasing(int[] arr1, int[] arr2) {
-        this.arr1=arr1;
-        this.arr2=arr2;
+        // this.arr1=arr1;
+        // this.arr2=arr2;
         Arrays.sort(arr2);
-        int result = solve(0, Integer.MIN_VALUE);
+        int result = solve(0, arr1, arr2, Integer.MIN_VALUE);
         return result == MAX_INT ? -1 : result;
     }
 }
