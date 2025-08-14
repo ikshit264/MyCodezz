7651c0d505722820c3f761776b042de8beb4de9d
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-08-14 10:10:09 +0530

    Time: 80 ms (23.76%), Space: 45 MB (27.44%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,23 +1,34 @@
 class Solution {
     static int[] nums;
-    Integer[][] store;
-    public int solve(int i, int prevIndex){
-        if (i >= nums.length) return 0;
-        if (store[i][prevIndex + 1] != null) return store[i][prevIndex + 1];
+    Integer[] store;
 
-        int notTake = solve(i+1, prevIndex);
-        int take = 0;
+    // public int solve(int i, int prevIndex){
+    //     if (i >= nums.length) return 0;
+    //     if (store[i][prevIndex + 1] != null) return store[i][prevIndex + 1];
 
-        if (prevIndex == -1 || nums[i] > nums[prevIndex]){
-            take = 1 + solve(i+1,i);
-        }
+    //     int notTake = solve(i+1, prevIndex);
+    //     int take = 0;
 
-        return store[i][prevIndex + 1] = Math.max(notTake, take);
-    }
+    //     if (prevIndex == -1 || nums[i] > nums[prevIndex]){
+    //         take = 1 + solve(i+1,i);
+    //     }
+
+    //     return store[i][prevIndex + 1] = Math.max(notTake, take);
+    // }
     public int lengthOfLIS(int[] nums) {
         this.nums = nums;
         int n = nums.length;
-        store = new Integer[n][n + 1];
-        return solve(0, -1);
+        store = new Integer[n];
+        java.util.Arrays.fill(store, 1);
+
+        int result = 1;
+        for (int i=0; i < n; i++){
+            for (int j = 0; j < i; j++){
+                if (nums[j] < nums[i])
+                    store[i] = Math.max(store[i], 1 + store[j]);
+            }
+            result = Math.max(result, store[i]);
+        }
+        return result;
     }
 }
