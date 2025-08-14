9a5505b4686cc1108e3dd3413de2fa6e81e5524b
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-08-14 09:09:32 +0530

    Time: 328 ms (20.13%), Space: 124 MB (16.88%) - LeetHub

---
+++ 1290-make-array-strictly-increasing/1290-make-array-strictly-increasing.java
@@ -0,0 +1,48 @@
+class Solution {
+    final int MAX_INT = 1_000_000_001;
+    int[] arr1, arr2;
+    Map<String, Integer> store = new HashMap<>();
+
+    private int upperBound(int[] arr, int target){
+        int left = 0, right = arr.length;
+
+        while(left < right){
+            int mid = left+(right-left)/2;
+            if (arr[mid] <= target) left = mid+1;
+            else right = mid;
+        }
+        return left;
+    }
+
+    public int solve(int i, int prev){
+        if (i >= arr1.length) return 0;
+
+        String key = i + "," + prev;
+        if (store.containsKey(key)) return store.get(key);
+        
+        int result1 = MAX_INT;
+
+        if (arr1[i] > prev) {
+            result1 = solve(i+1, arr1[i]);
+        }
+
+        int result2 = MAX_INT;
+
+        int j = upperBound(arr2, prev);
+        if (j < arr2.length)
+            result2 = 1 + solve(i+1, arr2[j]);
+
+
+        int ans = Math.min(result1, result2);
+        store.put(key, ans);
+        return ans;
+    }
+
+    public int makeArrayIncreasing(int[] arr1, int[] arr2) {
+        this.arr1=arr1;
+        this.arr2=arr2;
+        Arrays.sort(arr2);
+        int result = solve(0, Integer.MIN_VALUE);
+        return result == MAX_INT ? -1 : result;
+    }
+}
