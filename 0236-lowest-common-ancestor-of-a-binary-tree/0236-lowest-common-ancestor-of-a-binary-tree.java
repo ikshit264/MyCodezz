3d66478fc128237e0f0c3510a8be74a2cf3b945c
ikshit264 <126954198+ikshit264@users.noreply.github.com>
2025-08-31 00:07:59 +0530

    Time: 80 ms (5.09%), Space: 49.5 MB (5.63%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<TreeNode> store;
+
+    public boolean solve(TreeNode root, TreeNode target){
+        if (root == null) return false;
+        if (target == root) {
+            store.add(root);
+            System.out.print(root.val + " ");
+            return true;
+        }
+        boolean left = solve(root.left, target);
+        boolean right = solve(root.right, target);
+
+        if (right || left){
+            store.add(root);
+            System.out.print(root.val + " ");
+        }
+        return right || left;
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    store = new ArrayList<>();
+    solve(root, p);
+    List<TreeNode> pList = new ArrayList<>(store); // copy path for p
+    
+    store = new ArrayList<>();
+    solve(root, q);
+    List<TreeNode> qList = new ArrayList<>(store); // copy path for q
+
+    // Compare paths
+    int i = pList.size() - 1; // last = p itself
+    int j = qList.size() - 1; // last = q itself
+    TreeNode lca = null;
+
+    // Move backwards until paths diverge
+    while (i >= 0 && j >= 0 && pList.get(i) == qList.get(j)) {
+        lca = pList.get(i);
+        i--;
+        j--;
+    }
+    return lca;
+}
+
+}
